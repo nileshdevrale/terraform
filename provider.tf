32a326fc6598c5cf86985b835620057af17eba83
Nilesh Devrale <47170643+nileshdevrale@users.noreply.github.com>
2026-07-02 13:30:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "btp" {
   # For Global Account authentication
-  username   = var.btp_username
-  password   = var.btp_password
-  globalaccount = var.btp_global_account
+  username   = btp_username
+  password   = btp_password
+  globalaccount = btp_global_account
 }
