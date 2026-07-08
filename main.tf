07562cf0b6683ed25abfdac6a207553f7e0c370d
Nilesh Devrale <47170643+nileshdevrale@users.noreply.github.com>
2026-07-08 15:46:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "btp_subaccount" "terraformsubaccount"  {
+resource "btp_subaccount" "test"  {
   name        = "terraformsubaccount"
   subdomain   = "83c5527ftrial-ga" # Must be unique globally within your region
   region      = "us10"           # Example: "us10" (US East), "eu10" (Europe), "ap11" (Asia)
